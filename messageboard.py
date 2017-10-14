164bed2b9cb37fa4aae6907d292c94175e44823b
shubhamPrakashJha <prakash.shubhamjha@gmail.com>
2017-10-14 16:55:18 +0530

    define do_get method for request handler class

--- messageboard.py
+++ messageboard.py
@@ -35,6 +35,18 @@ class MessageHandler(BaseHTTPRequestHandler):
         self.send_header('Location', '/')
         self.end_headers()
 
+     def do_GET(self):
+
+        self.send_response(200)
+
+
+        self.send_header('Content-type', 'text/html; charset=utf-8')
+        self.end_headers()
+
+
+        mesg = form.format("\n".join(memory))
+        self.wfile.write(mesg.encode())
+
 
 
 if __name__ == '__main__':
