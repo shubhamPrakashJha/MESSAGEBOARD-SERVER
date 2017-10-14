f20804bac37bb583f8b4acad1745f4b2b77d7136
shubhamPrakashJha <prakash.shubhamjha@gmail.com>
2017-10-14 16:54:19 +0530

    define do_POST method for requst handler class

--- messageboard.py
+++ messageboard.py
@@ -16,6 +16,25 @@ form = '''<!DOCTYPE html>
 '''
 
 class MessageHandler(BaseHTTPRequestHandler):
+     def do_POST(self):
+
+        length = int(self.headers.get('Content-length', 0))
+
+
+        data = self.rfile.read(length).decode()
+        message = parse_qs(data)["message"][0]
+
+
+        message = message.replace("<", "&lt;")
+
+
+        memory.append(message)
+
+
+        self.send_response(303)
+        self.send_header('Location', '/')
+        self.end_headers()
+
 
 
 if __name__ == '__main__':
