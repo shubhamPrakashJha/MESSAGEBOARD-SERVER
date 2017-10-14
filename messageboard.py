c8e8a4c1cee8e7efbe97b08db5ef25053acd65d4
shubhamPrakashJha <prakash.shubhamjha@gmail.com>
2017-10-14 16:49:01 +0530

    add form's html page to form variable

--- messageboard.py
+++ messageboard.py
@@ -2,3 +2,15 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 from urllib.parse import parse_qs
 
 memory = []
+
+form = '''<!DOCTYPE html>
+  <title>Message Board</title>
+  <form method="POST">
+    <textarea name="message"></textarea>
+    <br>
+    <button type="submit">Post it!</button>
+  </form>
+  <pre>
+
+  </pre>
+'''
